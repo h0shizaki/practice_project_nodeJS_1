5add6c0e03cea8a7ebafa4ef21e7c7a3888c47de
Kitsune-fx <kant.cmd@gmail.com>
2021-11-18 16:56:49 +0700

    [🎮 Post Put book]

--- src/routes/books.js
+++ src/routes/books.js
@@ -33,6 +33,32 @@ const dbCon = require('../connection/connection')
 
     })
 
+    route.get('/edit/:id' , middleware.authAdmin , (req,res)=>{
+        const ID = req.params.id
+        if( ID === undefined) return res.send("ID is undefined").status(300);
+
+        dbCon.query("SELECT * FROM book where book_id = ?", ID ,(error,result,field)=>{
+            if(error) return res.send({error: true , message: error})
+            if(result.length == 0 || result === undefined) return res.send({error: true , message: "Book not found"})
+        
+            res.render('editbook.ejs' , {
+                title: "editbook",
+                message: "",
+                data: result[0]
+            })
+        })
+    })
+
+    route.post('/edit' , middleware.authAdmin, (req,res)=>{
+        dbCon.query("UPDATE book SET book_url = ?, book_name = ?, book_price = ?, book_status = ? WHERE book_id = ?",
+        [req.body.book_url, req.body.book_name, req.body.book_price, req.body.book_status, req.body.book_id],
+        (error,result,field) =>{
+            if(error) return res.status(400).send({error: true, message:error})
+
+            res.redirect('/books/dashboard');
+        })
+    })
+
 
 
 module.exports = route;
